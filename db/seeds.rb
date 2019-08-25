4f34c3ce1c52e0cdc926f287b7819570cbce3dc2
Daniela Sandoval <daniela.sandoval820@gmail.com>
2019-08-25 18:54:43 -0400

    m

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,8 +6,8 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.destroy_all
-Tone.destroy_all
 Adjective.destroy_all
+Tone.destroy_all
 Flower.destroy_all
 Bouquet.destroy_all
 
